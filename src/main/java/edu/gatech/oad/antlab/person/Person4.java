bb73a85e6e0086c8ff02fb09f9d20ca620342b06
Ananth Dandibhotla <anisaidan@gmail.com>
2019-01-22 15:35:26 -0500

    Completed Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Ananth Dandibhotla (ananth.dandibhotla@gatech.edu)
  *  @version 1.1
  */
 public class Person4 {
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      StringBuilder builder = new StringBuilder();
+      for(int i = 0; i < input.length(); i ++){
+        builder.append((char) (input.charAt(i)+1));
+      }
+      return builder.toString();
     }
     
     /**
